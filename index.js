b2a4e7eaa579db9d92d262c323fa3965661319a5
Escativ <escativity@gmail.com>
2024-10-21 14:42:31 +0300

    test finished

--- index.js
+++ index.js
@@ -1,30 +1,53 @@
-let x = 15;
-
-if (x >= 10 && x <= 20) {
-    console.log("Yra tarp 10 ir 20 (x: "+x+")");
-} else {
-    console.log("Kintamasis nėra tarp 10 ir 20.");
-}
+'use strict'
 
 
-let temperature = 15;
+// // 1stgit
 
-switch (true) {
-    case (temperature < 0):
-        console.log("It's cool");
-        break;
-    case (temperature >= 0 && temperature < 10):
-        console.log("Could be cooler");
-        break;
-    case (temperature >= 10 && temperature < 20):
-        console.log("Okay I can feel the heat");
-        break;
-    case (temperature >= 20 && temperature < 30):
-        console.log("");
-        break;
-    case (temperature >= 30):
-        console.log("Karšta! Saugokite save nuo saulės ir gerkite daug vandens.");
-        break;
-    default:
-        console.log("Temperatūra nežinoma.");
-}
+
+// let x = 15;
+
+// if (x >= 10 && x <= 20) {
+//     console.log("Yra tarp 10 ir 20 (x: "+x+")");
+// } else {
+//     console.log("Nėra tarp 10 ir 20.");
+// }
+
+
+// // 2nd
+
+
+// let temperature = 15;
+
+// switch (true) {
+//     case (temperature < 0):
+//         console.log("It's cool");
+//         break;
+//     case (temperature >= 0 && temperature < 10):
+//         console.log("Could be cooler");
+//         break;
+//     case (temperature >= 10 && temperature < 20):
+//         console.log("Okay I can feel the heat");
+//         break;
+//     case (temperature >= 20 && temperature < 30):
+//         console.log("It's getting way to warm");
+//         break;
+//     case (temperature >= 30):
+//         console.log("I dead.");
+//         break;
+//     default:
+//         console.log("Temperatūra nežinoma.");
+// }
+
+
+
+// // 3rd
+
+
+// let sideLength = prompt("Kvadroto kraštinės ilgis:");
+
+// if (sideLength > 0) {
+//     let area = Math.pow(sideLength, 2);
+//     alert("Kvadrato plotas yra: " + area);
+// } else {
+//     alert("Iveskite teigiama skaičiu.");
+// }
